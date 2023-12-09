77aadc7b57fe255f7cd2db9202003c2bfae0020a
Vivobook <viljami.narinen@aalto.fi>
2023-12-09 14:29:41 +0200

    layout started

--- client/src/App.js
+++ client/src/App.js
@@ -1,17 +1,28 @@
-import logo from "./logo.svg";
 import "./App.css";
 import { ChartComponent } from "./EnergyStorageGraph";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <ChartComponent />
-      </header>
+      <header className="App-header"></header>
+      <body className="App-body">
+        <div className="Split-Screen">
+          <div id="leftSide">
+            <h1>Graphs for energy stuff</h1>
+            <div id="chartsContainer">
+              <ChartComponent />
+              <ChartComponent />
+              <ChartComponent />
+            </div>
+          </div>
+          <div id="rightSide">
+            <div id="eventLogContainer">
+              <h2>Event Log</h2>
+              <div id="eventsAndAnomalies"></div>
+            </div>
+          </div>
+        </div>
+      </body>
     </div>
   );
 }
