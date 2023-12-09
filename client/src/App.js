b925defe1b2ee315e0de372e8b5eb61975f52bd3
Vivobook <viljami.narinen@aalto.fi>
2023-12-09 13:45:56 +0200

    chart.js switch

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,6 @@
 import logo from "./logo.svg";
 import "./App.css";
-import { EnergyStorageGraph } from "./components/EnergyStorageGraph";
+import { ChartComponent } from "./EnergyStorageGraph";
 
 function App() {
   return (
@@ -10,7 +10,7 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
-        <EnergyStorageGraph />
+        <ChartComponent />
       </header>
     </div>
   );
