ef575661afa4753532f8c3e0b702ba15dc3e2945
Antti Norrkniivilä <anttinorrk@Anttis-MacBook-Air.local>
2023-12-10 13:24:56 +0200

    Add data grid

--- src/App.js
+++ src/App.js
@@ -1,8 +1,23 @@
+import { useState } from "react";
 import "./App.css";
 import { ConsumptionGraph } from "./components/ConsumptionGraph";
 import { StorageAndSurplusGraph } from "./components/StorageAndSurplusGraph";
+import DataGrid, { DataGridEngine } from "./components/DataGrid";
 
 function App() {
+  const [latestDifference, setLatestDifference] = useState({
+    difference: 0,
+    current: 0,
+    baseline: 0
+})
+
+const handleSetDifference = (difference, current, baseline) => {
+    setLatestDifference({
+        difference: difference,
+        current: current,
+        baseline: baseline
+    })
+}
   return (
     <div className="App">
       <div id="page-container">
@@ -27,13 +42,26 @@ function App() {
             <div id="left-side">
               <h2>Graphs for energy stuff</h2>
               <div id="charts-container">
-                <ConsumptionGraph />
+              <ConsumptionGraph difference={latestDifference.difference} setDifferenceCallback={handleSetDifference} />
                 <StorageAndSurplusGraph />
               </div>
             </div>
             <div id="right-side">
               <div id="events-container">
-                <h2>Event Log</h2>
+              <h2>Key Data</h2>
+                                <DataGrid data={[
+                                    {label: 'Consumption to baseline (MWh)', value: latestDifference.difference, warning: latestDifference.difference >= 1000, good: latestDifference.difference <= -1000, extended: true},
+                                    {label: 'Total consumption (MWh)', value: latestDifference.current, baseline: latestDifference.baseline},
+                                    {label: 'Battery charge', value: '42 %'}
+                                    ]} />
+
+                                <DataGridEngine data={[
+                                    {label: 'Engine 1', value: 80, baseline: 80},
+                                    {label: 'Engine 2', value: 77, baseline: 80},
+                                    {label: 'Engine 3', value: 95, baseline: 0},
+                                ]}
+                                warning
+                                />
                 <div id="events-and-anomalies"></div>
               </div>
             </div>
