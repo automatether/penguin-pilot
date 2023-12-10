58b2711605ba8c71ae6001654c3d940dcfcba87b
automatether <83187517+automatether@users.noreply.github.com>
2023-12-10 14:06:19 +0200

    Update MapView.jsx
    yes

--- src/components/MapView.jsx
+++ src/components/MapView.jsx
@@ -2,8 +2,216 @@ import React from "react";
 
 const MapView = () => {
     return (
-        <div>
-            <h1>Map View</h1>
+        <div><table>
+	<tr>
+		<td></td>
+		<td>Trip ID</td>
+		<td>Ship type</td>
+		<td>Total power consumption</td>
+		<td>Power difference</td>
+		<td>Duration</td>
+		<td>Wave conditions</td>
+		<td>Wind conditions</td>
+		<td>Season</td>
+		<td>Total fuel consumption</td>
+		<td>Captain</td>
+	</tr>
+	<tr>
+		<td>Ship 1</td>
+		<td>AD-314</td>
+		<td>Cruise</td>
+		<td>1260.88</td>
+		<td>-239.12</td>
+		<td>171.52</td>
+		<td>9</td>
+		<td>3</td>
+		<td>Summer</td>
+		<td>24.33</td>
+		<td>O. Makela</td>
+	</tr>
+	<tr>
+		<td>Ship 1</td>
+		<td>BS-910</td>
+		<td>Cruise</td>
+		<td>1277.63</td>
+		<td>-222.37</td>
+		<td>161.02</td>
+		<td>7</td>
+		<td>2</td>
+		<td>Spring</td>
+		<td>28.04</td>
+		<td>O. Makela</td>
+	</tr>
+	<tr>
+		<td>Ship 3</td>
+		<td>RB-400</td>
+		<td>Cruise</td>
+		<td>1307.37</td>
+		<td>-292.63</td>
+		<td>174.31</td>
+		<td>3</td>
+		<td>2</td>
+		<td>Spring</td>
+		<td>41.47</td>
+		<td>O. Virtanen</td>
+	</tr>
+	<tr>
+		<td>Ship 1</td>
+		<td>IB-484</td>
+		<td>Cruise</td>
+		<td>1595.07</td>
+		<td>95.07</td>
+		<td>164.52</td>
+		<td>7</td>
+		<td>9</td>
+		<td>Autumn</td>
+		<td>25.57</td>
+		<td>O. Makela</td>
+	</tr>
+	<tr>
+		<td>Ship 5</td>
+		<td>XG-352</td>
+		<td>Container</td>
+		<td>1634.39</td>
+		<td>-365.61</td>
+		<td>196.34</td>
+		<td>6</td>
+		<td>5</td>
+		<td>Winter</td>
+		<td>44.52</td>
+		<td>O. Koskinen</td>
+	</tr>
+	<tr>
+		<td>Ship 2</td>
+		<td>ZT-101</td>
+		<td>Cruise</td>
+		<td>1723.56</td>
+		<td>223.56</td>
+		<td>160.57</td>
+		<td>9</td>
+		<td>6</td>
+		<td>Summer</td>
+		<td>36.15</td>
+		<td>H. Heikkinen</td>
+	</tr>
+	<tr>
+		<td>Ship 5</td>
+		<td>VL-915</td>
+		<td>Container</td>
+		<td>1937.71</td>
+		<td>-62.29</td>
+		<td>188.63</td>
+		<td>8</td>
+		<td>1</td>
+		<td>Winter</td>
+		<td>43.4</td>
+		<td>O. Koskinen</td>
+	</tr>
+	<tr>
+		<td>Ship 6</td>
+		<td>RZ-319</td>
+		<td>Tanker</td>
+		<td>2159.83</td>
+		<td>10.3</td>
+		<td>198.22</td>
+		<td>9</td>
+		<td>1</td>
+		<td>Autumn</td>
+		<td>48.74</td>
+		<td>M. Nieminen</td>
+	</tr>
+	<tr>
+		<td>Ship 5</td>
+		<td>PJ-541</td>
+		<td>Container</td>
+		<td>2337.06</td>
+		<td>50.12</td>
+		<td>195.17</td>
+		<td>7</td>
+		<td>7</td>
+		<td>Spring</td>
+		<td>49.84</td>
+		<td>O. Koskinen</td>
+	</tr>
+	<tr>
+		<td>Ship 3</td>
+		<td>TI-102</td>
+		<td>Cruise</td>
+		<td>2657.97</td>
+		<td>43.99</td>
+		<td>173.31</td>
+		<td>10</td>
+		<td>9</td>
+		<td>Summer</td>
+		<td>54.09</td>
+		<td>O. Virtanen</td>
+	</tr>
+	<tr>
+		<td>Ship 7</td>
+		<td>VO-860</td>
+		<td>Container</td>
+		<td>2869.47</td>
+		<td>176.43</td>
+		<td>190.13</td>
+		<td>3</td>
+		<td>6</td>
+		<td>Spring</td>
+		<td>54.89</td>
+		<td>J. Makela</td>
+	</tr>
+	<tr>
+		<td>Ship 2</td>
+		<td>HB-511</td>
+		<td>Cruise</td>
+		<td>3037.45</td>
+		<td>380.65</td>
+		<td>168.68</td>
+		<td>4</td>
+		<td>10</td>
+		<td>Spring</td>
+		<td>64.43</td>
+		<td>H. Heikkinen</td>
+	</tr>
+	<tr>
+		<td>Ship 7</td>
+		<td>QF-542</td>
+		<td>Container</td>
+		<td>3204.7</td>
+		<td>522.88</td>
+		<td>196.3</td>
+		<td>3</td>
+		<td>4</td>
+		<td>WInter</td>
+		<td>59.78</td>
+		<td>J. Makela</td>
+	</tr>
+	<tr>
+		<td>Ship 6</td>
+		<td>NV-510</td>
+		<td>Tanker</td>
+		<td>3373.59</td>
+		<td>705.04</td>
+		<td>192.67</td>
+		<td>6</td>
+		<td>4</td>
+		<td>Winter</td>
+		<td>67.78</td>
+		<td>M. Nieminen</td>
+	</tr>
+	<tr>
+		<td>Ship 7</td>
+		<td>JK-530</td>
+		<td>Container</td>
+		<td>3991.69</td>
+		<td>990.21</td>
+		<td>188.29</td>
+		<td>10</td>
+		<td>9</td>
+		<td>Autumn</td>
+		<td>73.54</td>
+		<td>J. Makela</td>
+	</tr>
+</table>
         </div>
     );
 };
