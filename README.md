829ee4922aeec822a9f4f1026e6ecef8a8094479
Vivobook <viljami.narinen@aalto.fi>
2023-12-10 11:05:27 +0200

    logo update

--- README.md
+++ README.md
@@ -7,3 +7,5 @@ ABB Marine Hackathon 2023
 Our team has developed a solution that allows the crew to monitor the current energy cosumption, generation and storage on the ship.
 
 In practice the solution is a web application that can be accessed by the crew or other relevant personnel. Providing the app with historic data starts the prediction model. The model is then used to predict the energy consumption, generation and storage, as well as calculate anomalies and other events.
+
+<div> Icons made by <a href="https://www.flaticon.com/authors/freepik" title="Freepik"> Freepik </a> from <a href="https://www.flaticon.com/" title="Flaticon">www.flaticon.com'</a></div>
--- client/src/App.js
+++ client/src/App.js
@@ -3,40 +3,48 @@ import { ConsumptionGraph } from "./components/ConsumptionGraph";
 import { StorageAndSurplusGraph } from "./components/StorageAndSurplusGraph";
 
 function App() {
-    return (
-        <div className="App">
-            <div id="page-container">
-                <div id="content-wrap">
-                    <header className="App-header">
-                        <h1>Binary Buccaneers' App</h1>
-                        <div id="nav-bar">
-                            <a href="index.html">Switch between views</a>
-                            <a href="description.html">Description</a>
-                            <a href="video.html">Video Presentation</a>
-                        </div>
-                    </header>
-                    <div className="Split-Screen">
-                        <div id="left-side">
-                            <h2>Graphs for energy stuff</h2>
-                            <div id="charts-container">
-                                <ConsumptionGraph />
-                                <StorageAndSurplusGraph />
-                            </div>
-                        </div>
-                        <div id="right-side">
-                            <div id="events-container">
-                                <h2>Event Log</h2>
-                                <div id="events-and-anomalies"></div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-                <footer id="footer">
-                    <p>Made by the Binary Buccaneers for the 2023 ABB Marine Hackathon</p>
-                </footer>
+  return (
+    <div className="App">
+      <div id="page-container">
+        <div id="content-wrap">
+          <header className="App-header">
+            <div className="logo-and-name">
+              <img
+                src="/logo192.png"
+                className="App-logo"
+                alt="logo"
+                id="logo"
+              />
+              <h1>Binary Buccaneers' App</h1>
             </div>
+            <div id="nav-bar">
+              <a href="index.html">Switch between views</a>
+              <a href="description.html">Description</a>
+              <a href="video.html">Video Presentation</a>
+            </div>
+          </header>
+          <div className="Split-Screen">
+            <div id="left-side">
+              <h2>Graphs for energy stuff</h2>
+              <div id="charts-container">
+                <ConsumptionGraph />
+                <StorageAndSurplusGraph />
+              </div>
+            </div>
+            <div id="right-side">
+              <div id="events-container">
+                <h2>Event Log</h2>
+                <div id="events-and-anomalies"></div>
+              </div>
+            </div>
+          </div>
         </div>
-    );
+        <footer id="footer">
+          <p>Made by the Binary Buccaneers for the 2023 ABB Marine Hackathon</p>
+        </footer>
+      </div>
+    </div>
+  );
 }
 
 export default App;
